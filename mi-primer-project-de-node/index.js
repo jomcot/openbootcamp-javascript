22c07f8aad0b903dfd090923974bb8105a92db93
jomcot <126102932+jomcot@users.noreply.github.com>
2023-03-05 23:51:02 +0100

    Add files via upload

---
+++ mi-primer-project-de-node/index.js
@@ -0,0 +1,7 @@
+// Este es el mensaje de bienveida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/* Esta es la parte intermedia*/
+
+// Este el mensaje de despedida
+console.log("adios");
