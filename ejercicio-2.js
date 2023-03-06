68501edc01c0469ce18677936e3d4737eac337d1
jomcot <126102932+jomcot@users.noreply.github.com>
2023-03-06 22:13:45 +0100

    Add files via upload

---
+++ ejercicio-2.js
@@ -0,0 +1,12 @@
+
+var nombre = "Jose Mario Cotes";
+var Edad = 36;
+var desarrollador = true;
+var nacimiento = 21-04-1986;
+
+const librofavorito = {
+    nombre: "Cien años de soledad",
+    autor: "Gabo",
+    fecha: 21-04-1986,
+    url: "www.google.com"
+}
